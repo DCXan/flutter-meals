5e165b298083b26be072550b21f6ea92d0fd1f4c
David Chen <chen.david.1016@gmail.com>
2024-02-27 15:37:35 -0600

    add details to meal item card

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_meals_app/models/meal.dart';
+import 'package:flutter_meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -10,12 +11,8 @@ class MealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
-      itemCount: meals.length,
-      itemBuilder: ((ctx, index) => Text(
-            meals[index].title,
-            style: const TextStyle(color: Colors.white),
-          )),
-    );
+        itemCount: meals.length,
+        itemBuilder: ((ctx, index) => MealItem(meal: meals[index])));
 
     if (meals.isEmpty) {
       content = Center(
